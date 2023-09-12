c36a020a18df01acf9c24a7de00be468b2ada718
nudova <gioibaothu@gmail.com>
2023-09-12 23:55:41 +0700

    Cài đặt full căn cơ, bắt đầu code Home

---
+++ lms_fe/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lms_fe/src/Components/Main.js
@@ -0,0 +1,10 @@
+import Home from './Components/Home';
+function Main() {
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
