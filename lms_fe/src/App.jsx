3ee2b818bb36d8d4756eaba45d8e72f03dbc0499
nudova <gioibaothu@gmail.com>
2023-09-15 09:02:45 +0700

    create SideNav

--- lms_fe/src/App.jsx
+++ lms_fe/src/App.jsx
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home';
+import './CSS/App.css';
+import SideNav from './Components/SideNav';
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <SideNav/>
     </div>
   );
 }
