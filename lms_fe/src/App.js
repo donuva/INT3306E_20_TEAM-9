aaae7d7a67bab077b8f167781f3ef3b6563172cd
quangnm-sonat <141127347+quangnm-sonat@users.noreply.github.com>
2023-09-16 10:05:42 +0700

    feature: done profile theme

--- lms_fe/src/App.js
+++ lms_fe/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import Profile from './Views/Profile';
 function App() {
   return (
     <div className="App">
      <Home/>
+     <Profile/>
     </div>
   );
 }
