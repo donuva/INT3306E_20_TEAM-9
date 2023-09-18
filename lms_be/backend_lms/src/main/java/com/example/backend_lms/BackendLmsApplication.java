d57f531e0b111082fdf2e48758d1cbe89259abf4
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-09-18 13:30:03 +0700

    1st commit

---
+++ lms_be/backend_lms/src/main/java/com/example/backend_lms/BackendLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLmsApplication.class, args);
+	}
+
+}
---
+++ lms_be/backend_lms/src/test/java/com/example/backend_lms/BackendLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
