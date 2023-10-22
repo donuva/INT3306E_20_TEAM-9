8a53d0292a5e1b22f60b32c2ed4ea37652ce325b
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-10-22 15:07:09 +0700

    Thêm auth

--- lms_be/backend_lms/src/main/java/com/example/backend_lms/controller/LoginController.java
+++ lms_be/backend_lms/src/main/java/com/example/backend_lms/controller/LoginController.java
@@ -1,4 +1,34 @@
 package com.example.backend_lms.controller;
 
+import com.example.backend_lms.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class LoginController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+        return ResponseEntity.ok(jwtService.createToken(username));
+    }
+
+    @PostMapping("/renewJwt")
+    public ResponseEntity<String> login(Principal p){
+        return ResponseEntity.ok(jwtService.createToken(p.getName()));
+    }
 }
---
+++ lms_be/backend_lms/src/main/java/com/example/backend_lms/service/JwtService.java
@@ -0,0 +1,39 @@
+package com.example.backend_lms.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("{jwt.secret:123456}")
+    private String secretKey;
+
+    public String createToken(String username){
+        Claims claims = Jwts.claims().setSubject(username);
+        Date now = new Date();
+        long validity = 5;
+        Date exp = new Date(now.getTime()+ validity *60*1000);
+
+        return Jwts.builder().setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
+    }
+
+    public String getUsername(String token){
+        try{
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        }catch (Exception ignored){
+
+        }
+        return "NOT FOUND";
+    }
+
+}
+
