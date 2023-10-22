93c70b0e508c1a7756d0d51be6d6764e108a06e0
nudova <gioibaothu@gmail.com>
2023-10-22 21:22:08 +0700

    Delete PageView

--- lms_fe/src/App.jsx
+++ lms_fe/src/App.jsx
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './CSS/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SideNav from './Components/SideNav';
-import Grade from './Components/StudentGrade';
-import CoursePage from './PageView/CoursePage';
-import ProfilePage from './PageView/ProfilePage';
 import Lecture from './Components/Lecture';
 import LectureDetail from './Components/LectureDetail';
-import CourseDetailPage from './PageView/CourseDetailPage';
 import Forum from './Components/Forum';
-import ForumPage from './PageView/ForumPage';
 import StudentGrade from './Components/StudentGrade';
-import StudentGradePage from './PageView/StudentGradePage';
-import TeacherGradePage from './PageView/TeacherGradePage';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import TeacherGrade from './Components/TeacherGrade';
+import Profile from './Components/Profile';
 
 
 
@@ -20,15 +16,16 @@ function App() {
   return (
     <div className="App">
      <Router>
-      {/* <CoursePage/> */}
+     <Header/>
       <Routes>
-        <Route path="/app/courses" element={<CoursePage/>} />
-        <Route path="/app/courseDetail" element={<CourseDetailPage/>} />
-        <Route path="/app/studentGrade" element={<StudentGradePage/>} />
-        <Route path="/app/teacherGrade" element={<TeacherGradePage/>} />
-        <Route path="/app/forum" element={<ForumPage/>} />
-        <Route path="/app/profile" element={<ProfilePage/>} />
+        <Route path="/app/courses" element={<Lecture/>} />
+        <Route path="/app/courseDetail" element={<LectureDetail/>} />
+        <Route path="/app/studentGrade" element={<StudentGrade/>} />
+        <Route path="/app/teacherGrade" element={<TeacherGrade/>} />
+        <Route path="/app/forum" element={<Forum/>} />
+        <Route path="/app/profile" element={<Profile/>} />
       </Routes>
+      <Footer/>
     </Router>
     </div>
   );
