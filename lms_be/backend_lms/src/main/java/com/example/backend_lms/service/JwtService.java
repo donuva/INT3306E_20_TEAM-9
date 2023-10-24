3b5c143e42bccb472b4ffe4685393b7349c6da84
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-10-24 09:08:54 +0700

    sua lay jwt.secret

--- lms_be/backend_lms/src/main/java/com/example/backend_lms/service/JwtService.java
+++ lms_be/backend_lms/src/main/java/com/example/backend_lms/service/JwtService.java
@@ -16,7 +16,7 @@ public class JwtService {
     public String createToken(String username){
         Claims claims = Jwts.claims().setSubject(username);
         Date now = new Date();
-        long validity = 5;
+        long validity = 10;
         Date exp = new Date(now.getTime()+ validity *60*1000);
 
         return Jwts.builder().setClaims(claims)
