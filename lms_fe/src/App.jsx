e76ee725f61edfcefb30c5581f62de3804fbf813
nudova <gioibaothu@gmail.com>
2023-10-31 10:47:49 +0700

    DONE SEARCHBOX

--- lms_fe/src/App.jsx
+++ lms_fe/src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './CSS/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lecture from './Components/Lecture';
@@ -9,24 +8,27 @@ import Header from './Components/header';
 import Footer from './Components/footer';
 import TeacherGrade from './Components/TeacherGrade';
 import Profile from './Components/Profile';
-
-
+import ListofResult from './Components/ListofResult';
+import { AppProvider } from './Components/AppContext'; // Import AppProvider
 
 function App() {
   return (
     <div className="App">
-     <Router>
-     <Header/>
-      <Routes>
-        <Route path="/app/courses" element={<Lecture/>} />
-        <Route path="/app/courseDetail" element={<LectureDetail/>} />
-        <Route path="/app/studentGrade" element={<StudentGrade/>} />
-        <Route path="/app/teacherGrade" element={<TeacherGrade/>} />
-        <Route path="/app/forum" element={<Forum/>} />
-        <Route path="/app/profile" element={<Profile/>} />
-      </Routes>
-      <Footer/>
-    </Router>
+      <Router>
+        <AppProvider> {/* Wrap tất cả các thành phần trong AppProvider */}
+          <Header />
+          <Routes>
+            <Route path="/app/courses" element={<Lecture />} />
+            <Route path="/app/courseDetail" element={<LectureDetail />} />
+            <Route path="/app/studentGrade" element={<StudentGrade />} />
+            <Route path="/app/teacherGrade" element={<TeacherGrade />} />
+            <Route path="/app/forum" element={<Forum />} />
+            <Route path="/app/profile" element={<Profile />} />
+            <Route path="/app/listofResult" element={<ListofResult data={"nu"} />} />
+          </Routes>
+          <Footer />
+        </AppProvider>
+      </Router>
     </div>
   );
 }
---
+++ lms_fe/src/Components/AppContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const updateSearchValue = (value) => {
+    setSearchValue(value);
+  };
+
+  return (
+    <AppContext.Provider value={{ searchValue, updateSearchValue }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContext, AppProvider };
