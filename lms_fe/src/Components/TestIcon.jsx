a227c7023de3d1c77f954af74c443f68585fe6d6
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-11-03 17:54:30 +0700

    them sign out

--- lms_fe/src/Components/TestIcon.jsx
+++ lms_fe/src/Components/TestIcon.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Dropdown, Avatar, Menu } from 'antd'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 
 const TestIcon = () => {
-  
-  const clickHandler = ({ key }) => {
-    
+
+  const navigate = useNavigate();
+  const clickHandler = () => {
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('user');
+    localStorage.removeItem('student_id');
+    localStorage.removeItem('teacher_id');
+    navigate('/login');
   }
 
   const menu = (
@@ -14,7 +19,7 @@ const TestIcon = () => {
       <Menu.Item key="PROFILE">
         <NavLink to={'/app/profile'}>My Profile</NavLink>
       </Menu.Item>
-      <Menu.Item key="SIGN_OUT">Sign out</Menu.Item>
+      <Menu.Item onClick={clickHandler} key="SIGN_OUT">Sign out</Menu.Item>
     </Menu>
   )
 
@@ -27,7 +32,7 @@ const TestIcon = () => {
       arrow
     >
       <Avatar src={'https://www.bing.com/th?id=OIP.WtYTJGHZ3_-_r2vkTdPEpgHaGG&w=150&h=123&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2'}
-       style={{ cursor: 'pointer', marginRight: '10px' }}></Avatar>
+        style={{ cursor: 'pointer', marginRight: '10px' }}></Avatar>
     </Dropdown>
   )
 }
