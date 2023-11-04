87abc69820bc25f99326830bd6c0a51eefdcf00b
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-11-04 23:32:14 +0700

    Sửa courseDetail, thiếu truy cập link cho từng item, thiếu nút add cho giáo viên

--- lms_fe/src/Components/CourseDetail.jsx
+++ lms_fe/src/Components/CourseDetail.jsx
@@ -1,35 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Collapse, Badge, Button, Card, List } from 'antd';
+import { Collapse, Badge, Button, Card, List, Menu } from 'antd';
 import {
   YoutubeFilled,
   NotificationOutlined,
   FileTextOutlined,
   CommentOutlined,
   BarChartOutlined,
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  RightOutlined,
+  LeftOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import MenuItem from 'antd/es/menu/MenuItem';
 
-const customStyle = {
-  textAlign: 'left',
-};
-
-
-
-const CustomPanel = ({ label, children, addButton, isOpen, togglePanel }) => (
-  <div className="custom-panel">
-    <div className="custom-label" style={{ textAlign: 'left' }} onClick={togglePanel}>
-      {label}
-    </div>
-    {isOpen && (
-      <div style={{ textAlign: 'left', paddingLeft: '24px', paddingTop: '24px' }}>
-        {addButton}
-        {children}
-      </div>
-    )}
-  </div>
-);
+const { SubMenu } = Menu;
 
 const CourseDetail = ({ checkTokenExpiration }) => {
   const navigate = useNavigate();
@@ -45,6 +37,10 @@ const CourseDetail = ({ checkTokenExpiration }) => {
     role: '',
   });
   const { cid } = useParams();
+  const [course, setCourse] = useState({});
+  const [exerciseList, setExerciseList] = useState([]);
+  const [lessonList, setLessonList] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
 
   useEffect(() => {
     if (!checkTokenExpiration()) {
@@ -55,153 +51,78 @@ const CourseDetail = ({ checkTokenExpiration }) => {
     }
   }, []);
 
-  const [notificationNumber, setNotificationNumber] = useState(0);
-  const [notification, setNotification] = useState([]);
-  const [Course_id, setCourse_id] = useState(1)
-
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:8080/lms/course/notification?course_id=4&current_page=0')
-  //     .then((response) => {
-  //       setNotificationNumber(response.data.totalElements);
-  //       setNotification([...notification, ...response.data.data]);
-  //       console.log('đây là notification');
-  //       console.log(notification);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error fetching data: ', error);
-  //     });
-  // }, []);
-
-  const [lectureData, setLectureData] = useState([]);
-  //ĐANG FIX course_id=1 vì bên course chưa làm xong , chưa truyền id được
-
   useEffect(() => {
-    const getCourseById = async () => {
-      try {
-        const config = {
-          method: 'get',
-          maxBodyLength: Infinity,
-          url: 'http://localhost:8080/lms/course/2',
-          headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('jwt')
-          },
-        };
-
-        const response = await axios.request(config);
-        // Cập nhật lại state sau khi lưu thành công
-        // setUserData(response.data);
-        console.log(response.data)
-      } catch (error) {
-        console.log(userData.id)
-        console.log(error);
-      }
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `http://localhost:8080/lms/course/${cid}`,
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+      },
     };
-    getCourseById();
-  }, [Course_id]);
 
-  const [activePanel, setActivePanel] = useState(null);
+    axios.request(config).then((response) => {
+      setCourse(response.data);
+      setExerciseList(response.data.exerciseList);
+      setLessonList(response.data.lessonList);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
-
-  // // Fake data for lecture items
-  // const lectureData = [
-  //   { title: 'Lecture 1: Introduction to React', file: 'intro_to_react.pdf' },
-  //   { title: 'Lecture 2: State and Props', file: 'state_and_props.pdf' },
-  //   { title: 'Lecture 3: Components and Lifecycle', file: 'components_lifecycle.pdf' },
-  // ];
-
-  const togglePanel = (panelKey) => {
-    setActivePanel((prevActivePanel) => (prevActivePanel === panelKey ? null : panelKey));
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
   };
 
   return (
-    <Collapse
-      defaultActiveKey={['1', '2', '3', '4']}
-      activeKey={activePanel}
-      onChange={togglePanel}
-      style={customStyle}
-    >
-      <CustomPanel isOpen={true} togglePanel={() => { }}>
-        <Link to={`/app/courses/${cid}/forum`} style={{ paddingLeft: '4px', display: 'flex', border: '1px solid #000', width: '80px', borderRadius: '8px', backgroundColor: '#F3BE0F' }}>
-          <p>Conversation</p>
-          <CommentOutlined />
-        </Link>
+    <div style={{ display: 'flex', height: '1000px' }}>
+      <div className='sidenav' style={{ width: collapsed ? 80 : 256, backgroundColor: '#001529' }}>
 
-        <Link to="/app/studentGrade" style={{ paddingLeft: '4px', display: 'flex', border: '1px solid #000', width: '80px', borderRadius: '8px', backgroundColor: '#F3BE0F' }}>
-          <p>Grade</p>
-          <BarChartOutlined />
-        </Link>
-      </CustomPanel>
+        <Menu
 
-      <Collapse.Panel
-        key="2"
-        header={
-          <CustomPanel label={<><NotificationOutlined style={{ marginRight: '8px' }} /> Thông Báo</>} addButton={null} isOpen={activePanel === '2'} togglePanel={() => togglePanel('2')}>
-            <Badge count={notification.length} showZero>
-              <NotificationOutlined style={{ marginRight: '8px' }} />
-            </Badge>
-          </CustomPanel>
-        }
-      >
-        <Link to={`/app/courses/${cid}/addNoti`} style={{ paddingLeft: '4px', display: 'flex', border: '1px solid #000', width: '120px', borderRadius: '8px', backgroundColor: '#F3BE0F' }}>
-          <p>Notification</p>
-          <NotificationOutlined />
-        </Link>
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+        >
+          <Button onClick={toggleCollapsed} style={{ marginBottom: 16, backgroundColor: '#001529', color: 'white', border: '0px' }}>
+            {collapsed ? <RightOutlined /> : <LeftOutlined />}
+          </Button>
+          <Menu.Item key="1" icon={<AppstoreOutlined />}>
+            <Link to={`/app/courses/${cid}/notifications`}>Notifications</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<FileTextOutlined />}>
+            <Link to="/app/course/studentGrade">Grade</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<CommentOutlined />}>
+            <Link to={`/app/courses/${cid}/forum`}>Forum</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
 
-        {notification.map((notification) => (
-          <Card
-            size="small"
-            title={
-              <span style={{ display: 'flex', alignContent: 'left' }}>
-                <span style={{ paddingTop: '10px', paddingLeft: '10px' }}>{' ' + notification.topic}</span>
-              </span>
-            }
-          >
-            <div style={{ textAlign: 'left' }} className="commentData">
-              {notification.msg}
-            </div>
-          </Card>
-        ))}
-      </Collapse.Panel>
-
-      <Collapse.Panel
-        key="3"
-        header={
-          <CustomPanel label={<><FileTextOutlined style={{ marginRight: '8px' }} /> Bài Giảng</>} addButton={null} isOpen={activePanel === '3'} togglePanel={() => togglePanel('3')} />
-        }
-      >
-        <Link to="/app/addLesson" style={{ paddingLeft: '4px', display: 'flex', border: '1px solid #000', width: '120px', borderRadius: '8px', backgroundColor: '#F3BE0F' }}>
-          <p>Add Bài Giảng</p>
-          <FileTextOutlined />
-        </Link>
-        <FileTextOutlined style={{ marginRight: '8px' }} />
+      <div style={{ flex: 1 }}>
         <List
-          dataSource={lectureData}
+          header="Exercise List"
+          bordered
+          dataSource={exerciseList}
           renderItem={(item) => (
             <List.Item>
-              <List.Item.Meta title={<Button type="link">{item.title}</Button>} />
-              <FileTextOutlined style={{ marginLeft: '8px' }} />
+              {item.title}
             </List.Item>
           )}
         />
-      </Collapse.Panel>
 
-
-      <Collapse.Panel
-        key="4"
-        header={
-          <CustomPanel label={<><YoutubeFilled style={{ marginRight: '8px' }} /> Bài Tập</>} addButton={null} isOpen={activePanel === '4'} togglePanel={() => togglePanel('4')} />
-        }
-      >
-        <Link to="/app/addExercise" style={{ paddingLeft: '4px', display: 'flex', border: '1px solid #000', width: '120px', borderRadius: '8px', backgroundColor: '#F3BE0F' }}>
-          <p>Add Bài Tập</p>
-          <YoutubeFilled />
-        </Link>
-        <YoutubeFilled style={{ marginRight: '8px' }} />
-        <Button type="link">Chuyển tới trang nộp bài tập</Button>
-      </Collapse.Panel>
-
-    </Collapse>
+        <List
+          header="Lesson List"
+          bordered
+          dataSource={lessonList}
+          renderItem={(item) => (
+            <List.Item>
+              {item.topic}
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   );
 };
 
