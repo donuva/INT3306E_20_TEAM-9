6afd77d2a64f3abf7e21a4c91e127f9f10d3739a
hanguyenanhson <hanguyenanhson@gmail.com>
2023-11-05 10:32:37 +0700

    all notification

---
+++ lms_fe/src/Components/AllNotification.jsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react'
+import { Card, Button, Input } from 'antd'
+import Meta from 'antd/lib/card/Meta'
+import Avatar from 'antd/lib/avatar/avatar'
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+// import AllComments from './commentCard'
+// import {
+//   removeDiscussion,
+//   addComment
+// } from '../../../reducers/discussionReducer'
+// import './../styles.css'
+
+function Notification({ checkTokenExpiration }) {
+
+  // const dispatch = useDispatch()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = searchParams.get("page") || 0;
+  const [notification, setNotification] = useState([]);
+  const { cid } = useParams();
+  const [pageInfo, setPageInfo] = useState({});
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (!checkTokenExpiration()) {
+      alert('You need to re-login');
+      navigate('/login');
+    }
+    axios
+      .get(`http://localhost:8080/lms/course/notification?course_id=${cid}` + "&current_page=" + page, {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+        }
+      })
+      .then((response) => {
+
+        setNotification(response.data.data); // Lưu trữ dữ liệu lấy từ API vào state
+        setPageInfo(response.data);
+
+        console.log("đây là noti ")
+        console.log(notification)
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [page,]
+  );
+  const renderPagination = () => {
+    return (
+      <nav className="footer">
+        {pageInfo.totalPages != 0 &&
+          <ul className="pagination justify-content-center">
+
+            {page > 0 && (
+              <li className='page-item' onClick={() => handlePageChange(page - 1)} ><Link style={{}} className='page-link' >Previous</Link></li>
+            )}
+            {pageInfo.totalPages &&
+              Array.from({ length: pageInfo.totalPages }, (_, index) => (
+                <li
+                  onClick={() => handlePageChange(index)}
+                  key={index}
+                  className={page === index ? 'page-item active' : 'page-item'}
+                >
+                  <Link className='page-link' >{index + 1}</Link>
+
+
+                </li>
+              ))}
+            {page < pageInfo.totalPages - 1 && (
+              <li onClick={() => handlePageChange(page + 1)} className='page-item' ><Link className='page-link' >Next</Link> </li>
+            )}
+
+          </ul>}
+      </nav>
+    )
+
+  }
+
+  const handlePageChange = (newPage) => {
+    setSearchParams({ page: newPage });
+  };
+
+  return (
+    <div className="container" style={{ marginTop: '20px', marginBottom: '20px' }}>
+      <Card
+        hoverable
+        className="customcard"
+        title={
+          <div style={{ backgroundColor: 'orange', textAlign: 'center' }}>
+            <Meta
+
+              title="Notification"
+            />
+
+          </div>
+        }
+      >
+
+        <Card
+          size="small"
+          type="inner"
+          className="commentcard"
+          title="It's Notifiying time!"
+        >
+          {notification.map((notification) => (
+            <Card
+              size="small"
+              title={
+                <span style={{ display: "flex", alignContent: "left" }} >
+                  <span style={{ paddingTop: "10px"}}>{' ' + notification.topic}</span>
+                </span>
+              }
+            >
+              <div style={{ textAlign: "left" }} className='notificationData'>{notification.msg}</div>
+
+            </Card>
+          ))}
+          {renderPagination()}
+        </Card>
+      </Card>
+    </div>
+  )
+}
+
+export default Notification;
