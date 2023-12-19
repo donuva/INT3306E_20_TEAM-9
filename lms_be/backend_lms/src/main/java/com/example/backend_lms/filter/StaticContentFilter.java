ebbe70cfcbbc24600390bc730217ae23f834541e
Huy Nguyen <huynguyenquang030803@gmail.com>
2023-12-19 08:11:56 +0700

    update

--- lms_be/backend_lms/src/main/java/com/example/backend_lms/filter/StaticContentFilter.java
+++ lms_be/backend_lms/src/main/java/com/example/backend_lms/filter/StaticContentFilter.java
@@ -14,6 +14,8 @@ import java.util.List;
 @Component
 public class StaticContentFilter implements Filter {
 
+    private List<String> fileExtensions = Arrays.asList("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "appcache", "jpg", "jpeg", "gif", "ico", "pdf");
+
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -24,11 +26,15 @@ public class StaticContentFilter implements Filter {
         String path = request.getServletPath();
 
         boolean isApi = path.startsWith("/api");
-        boolean isResourceFile = path.startsWith("/storage");
+        boolean isStorage = path.startsWith("/storage");
+        boolean isResourceFile = !isApi && fileExtensions.stream().anyMatch(path::contains) && !isStorage;
+
 
         if (isApi) {
             chain.doFilter(request, response);
         } else if (isResourceFile) {
+            resourceToResponse("static" + path, response);
+        } else if (isStorage) {
             chain.doFilter(request, response);
         } else {
             resourceToResponse("static/index.html", response);
