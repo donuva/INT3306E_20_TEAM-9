0d1ff94160f3dc254b803c21683b3b7dcc2458d1
huydev38 <huynguyenquang030803@gmail.com>
2023-12-20 10:22:56 +0700

    update giao dien

--- lms_fe/src/Components/Sidebar.jsx
+++ lms_fe/src/Components/Sidebar.jsx
@@ -34,38 +34,38 @@ export default function Sidebar({ cid, isTeacher, selected }) {
                     {collapsed ? <RightOutlined /> : <LeftOutlined />}
                 </Button>
                 <Menu.Item key="0" icon={<AppstoreOutlined />}>
-                    <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}`}>
+                    <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}`}>
                         Course
                     </Link>
                 </Menu.Item>
                 <Menu.Item key="1" icon={<NotificationOutlined />}>
-                    <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}/notifications`}>Notifications</Link>
+                    <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}/notifications`}>Notifications</Link>
                 </Menu.Item>
                 {/* sua lai theo teacher hay student thi da link tuong ung */}
                 <Menu.Item key="2" icon={<DashboardOutlined />}>
                     {isTeacher === true ? (
-                        <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}/teacherGrade`}>Grade</Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}/teacherGrade`}>Grade</Link>
                     ) : (
-                        <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}/studentGrade`}>Grade</Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}/studentGrade`}>Grade</Link>
                     )}
                 </Menu.Item>
                 <Menu.Item key="3" icon={<CommentOutlined />}>
-                    <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}/forum`}>Forum</Link>
+                    <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}/forum`}>Forum</Link>
                 </Menu.Item>
                 {isTeacher === true &&
                     <Menu.Item key="4" icon={<ExperimentOutlined />}>
-                        <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/addExercise/${cid}`}>New Exercise</Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/app/addExercise/${cid}`}>New Exercise</Link>
                     </Menu.Item>
                 }
                 {isTeacher === true &&
                     <Menu.Item key="5" icon={<FileTextOutlined />}>
-                        <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/addLesson/${cid}`}>New Lesson</Link>
+                        <Link style={{ textDecoration: 'none'}} to={`/app/addLesson/${cid}`}>New Lesson</Link>
                     </Menu.Item>
                 }
 
                 {isTeacher === true &&
                     <Menu.Item key="6" icon={<UserAddOutlined />}>
-                        <Link style={{ textDecoration: 'none', color: '#3A4D39' }} to={`/app/courses/${cid}/enroll-request`}>Request List</Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/app/courses/${cid}/enroll-request`}>Request List</Link>
                     </Menu.Item>
                 }
 
